c5eb95a166aa525b337c6440fee8b996b680d846
Intan <waodeintansss@gmail.com>
2019-12-20 21:26:55 +0800

    menampilkan movie

---
+++ js/script.js
@@ -0,0 +1,37 @@
+$('#search-button').on('click', function() {
+    $.ajax({
+        url: 'http://omdbapi.com',
+        type: 'get',
+        dataType: 'json',
+        data: {
+            'apikey' : '515dc8ff',
+            's' : $('#search-input').val()
+        },
+        success: function (result) {
+            if (result.Response == "True") {
+                let movies = result.Search;
+
+                $.each(movies, function(i, data) {
+                    $('#movie-list').append(`
+                        <div class="col-md-4">
+                            <div class="card">
+                                <img src="`+ data.Poster +`" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                <h5 class="card-title">`+ data.Title +`</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">`+ data.Year +`</h5>
+                                <a href="#" class="card-link">See Detail</a>
+                                </div>
+                            </div>
+                        </div>
+                    `);
+                });
+            } else {
+                $('#movie-list').html(`
+                    <div class="col">
+                        <h1 class="text-center">` + result.Error + `</h1>
+                    </div>
+                `)
+            }
+        }
+    });
+});
